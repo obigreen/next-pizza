dcc8a935caab3517e1ad3a4ae3a2a5da40b819a4
Sergey Budyakin <youshtuchka@gmail.com>
2026-04-20 19:53:12 +0300

    Move app to src, add Tweet types and component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import {Tweet} from "@/src/app/(home)/Tweet";
+
+export default function Home() {
+    return (
+        <Tweet tweet={{
+            author: 'Saaas',
+            text: 'Serega'
+        }}/>
+    );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+    display: "swap"
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+    display: "swap"
+});
+
+export const metadata: Metadata = {
+    title: "X App",
+    description: "Frontend Development",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html
+            lang="en"
+         className={''}
+        >
+        <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>{children}</body>
+        </html>
+    );
+}
